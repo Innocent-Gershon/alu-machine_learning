4e4cfde1351171b2b14e1228c9867bf42b7e7a3e
Innocent-Gershon <i.nangah@gmail.com>
2025-09-12 14:09:22 +0200

    Flip Me Over

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -4,6 +4,7 @@ Module for transposing a 2D matrix.
 
 This module provides the function `matrix_transpose` which returns
 a new matrix that is the transpose of the given 2D matrix.
+
 """
 
 def matrix_transpose(matrix):
@@ -17,4 +18,3 @@ def matrix_transpose(matrix):
         list of lists: A new matrix representing the transpose of `matrix`.
     """
     return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
-
