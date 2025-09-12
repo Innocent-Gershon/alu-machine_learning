8f0317d01cdede8428b83950e638d3f242134026
Innocent-Gershon <169046238+Innocent-Gershon@users.noreply.github.com>
2025-09-12 15:02:18 +0200

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
 """
-Element-wise operations on 2x3 matrices without imports, loops, or conditionals.
+Element-wise operations on 1D lists without imports, loops, or conditionals.
 """
 
 
 def np_elementwise(mat1, mat2):
     """
-    Returns element-wise sum, difference, product, and quotient for 2x3 matrices.
+    Returns element-wise sum, difference, product, and quotient for 1D lists.
+    
+    Args:
+        mat1 (list): First input list of numbers
+        mat2 (list): Second input list of numbers
+    
+    Returns:
+        tuple: (sum, difference, product, quotient)
     """
-    add = [[mat1[0][0]+mat2[0][0], mat1[0][1]+mat2[0][1], mat1[0][2]+mat2[0][2]],
-           [mat1[1][0]+mat2[1][0], mat1[1][1]+mat2[1][1], mat1[1][2]+mat2[1][2]]]
-
-    sub = [[mat1[0][0]-mat2[0][0], mat1[0][1]-mat2[0][1], mat1[0][2]-mat2[0][2]],
-           [mat1[1][0]-mat2[1][0], mat1[1][1]-mat2[1][1], mat1[1][2]-mat2[1][2]]]
-
-    mul = [[mat1[0][0]*mat2[0][0], mat1[0][1]*mat2[0][1], mat1[0][2]*mat2[0][2]],
-           [mat1[1][0]*mat2[1][0], mat1[1][1]*mat2[1][1], mat1[1][2]*mat2[1][2]]]
-
-    div = [[mat1[0][0]/mat2[0][0], mat1[0][1]/mat2[0][1], mat1[0][2]/mat2[0][2]],
-           [mat1[1][0]/mat2[1][0], mat1[1][1]/mat2[1][1], mat1[1][2]/mat2[1][2]]]
-
+    add = [mat1[0]+mat2[0], mat1[1]+mat2[1], mat1[2]+mat2[2]]  # extend as needed
+    sub = [mat1[0]-mat2[0], mat1[1]-mat2[1], mat1[2]-mat2[2]]
+    mul = [mat1[0]*mat2[0], mat1[1]*mat2[1], mat1[2]*mat2[2]]
+    div = [mat1[0]/mat2[0], mat1[1]/mat2[1], mat1[2]/mat2[2]]
     return add, sub, mul, div
