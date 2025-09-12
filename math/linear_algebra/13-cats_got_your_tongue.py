9a9014c8038f41c954b8ff47b386f6f981252185
Innocent-Gershon <i.nangah@gmail.com>
2025-09-12 15:11:45 +0200

    Cats Got Your Tongue

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Concatenates two numpy matrices along a specified axis.
+"""
+
+
+import numpy as np
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    Concatenates two numpy matrices along a given axis.
+
+    Args:
+        mat1 (np.ndarray): First input matrix.
+        mat2 (np.ndarray): Second input matrix.
+        axis (int, optional): Axis along which to concatenate. Defaults to 0.
+
+    Returns:
+        np.ndarray: New matrix resulting from concatenation.
+    """
+    return np.concatenate((mat1, mat2), axis=axis)
