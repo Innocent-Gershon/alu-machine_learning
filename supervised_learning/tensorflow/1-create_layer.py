5bfbcbf16975909c9b92eb7307115675464eb710
Innocent-Gershon <169046238+Innocent-Gershon@users.noreply.github.com>
2026-01-30 07:10:01 -0500

    Done

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Create layer"""
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    Creates a layer for a neural network
+
+    Args:
+        prev: tensor output of the previous layer
+        n: number of nodes in the layer to create
+        activation: activation function that the layer should use
+
+    Returns:
+        tensor output of the layer
+    """
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.dense(prev, n, activation=activation,
+                            kernel_initializer=init,
+                            name='layer')
+    return layer
