19533f8e483e41f014eecf058e3a4b62e85f5540
Innocent-Gershon <169046238+Innocent-Gershon@users.noreply.github.com>
2026-05-25 20:25:59 -0400

    Refactor NST class and improve image scaling

--- neural_style_transfer/0-neural_style.py
+++ neural_style_transfer/0-neural_style.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-"""
-0-neural_style.py
-"""
+"""Neural Style Transfer module"""
+
 import numpy as np
 import tensorflow as tf
 
 
 class NST:
-    """
-    Neural Style Transfer class
-    """
+    """NST class for neural style transfer"""
 
     style_layers = [
         'block1_conv1',
@@ -23,42 +20,34 @@ class NST:
 
     def __init__(self, style_image, content_image,
                  alpha=1e4, beta=1):
-        """
-        Class constructor
-        """
+        """Class constructor"""
 
-        # Validate style_image
         if (not isinstance(style_image, np.ndarray) or
                 len(style_image.shape) != 3 or
                 style_image.shape[2] != 3):
             raise TypeError(
-                "style_image must be a numpy.ndarray with shape (h, w, 3)"
+                "style_image must be a numpy.ndarray "
+                "with shape (h, w, 3)"
             )
 
-        # Validate content_image
         if (not isinstance(content_image, np.ndarray) or
                 len(content_image.shape) != 3 or
                 content_image.shape[2] != 3):
             raise TypeError(
-                "content_image must be a numpy.ndarray with shape (h, w, 3)"
+                "content_image must be a numpy.ndarray "
+                "with shape (h, w, 3)"
             )
 
-        # Validate alpha
         if (not isinstance(alpha, (int, float)) or alpha < 0):
             raise TypeError(
                 "alpha must be a non-negative number"
             )
 
-        # Validate beta
         if (not isinstance(beta, (int, float)) or beta < 0):
             raise TypeError(
                 "beta must be a non-negative number"
             )
 
-        # Enable eager execution
-        tf.compat.v1.enable_eager_execution()
-
-        # Set instance attributes
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
         self.alpha = alpha
@@ -66,45 +55,35 @@ class NST:
 
     @staticmethod
     def scale_image(image):
-        """
-        Rescales an image such that its pixels values are between 0 and 1
-        and its largest side is 512 pixels
-        """
+        """Scales image"""
 
-        # Validate image
         if (not isinstance(image, np.ndarray) or
                 len(image.shape) != 3 or
                 image.shape[2] != 3):
             raise TypeError(
-                "image must be a numpy.ndarray with shape (h, w, 3)"
+                "image must be a numpy.ndarray "
+                "with shape (h, w, 3)"
             )
 
         h, w, _ = image.shape
 
-        # Compute new dimensions
-        max_dim = 512
-
         if h > w:
-            new_h = max_dim
-            new_w = int((w / h) * max_dim)
+            new_h = 512
+            new_w = int(w * 512 / h)
         else:
-            new_w = max_dim
-            new_h = int((h / w) * max_dim)
+            new_w = 512
+            new_h = int(h * 512 / w)
 
-        # Resize image using bicubic interpolation
-        resized = tf.image.resize(
+        image = tf.image.resize(
             image,
             (new_h, new_w),
             method=tf.image.ResizeMethod.BICUBIC
         )
 
-        # Scale pixel values to [0, 1]
-        resized = resized / 255.0
+        image = image / 255
 
-        # Clip values to ensure range [0, 1]
-        resized = tf.clip_by_value(resized, 0.0, 1.0)
+        image = tf.clip_by_value(image, 0, 1)
 
-        # Add batch dimension
-        resized = tf.expand_dims(resized, axis=0)
+        image = tf.expand_dims(image, axis=0)
 
-        return resized
+        return image
